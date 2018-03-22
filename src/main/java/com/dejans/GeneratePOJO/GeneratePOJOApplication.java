f2314fdfb74475e9e201f1bbc0965a96638596fe
dejansmi <smiljanic_dejan@hotmail.com>
2018-03-23 00:13:17 +0100

    Object generate file

--- src/main/java/com/dejans/GeneratePOJO/GeneratePOJOApplication.java
+++ src/main/java/com/dejans/GeneratePOJO/GeneratePOJOApplication.java
@@ -1,6 +1,8 @@
 
 package com.dejans.GeneratePOJO;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,8 +10,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GeneratePOJOApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GeneratePOJOApplication.class, args);
+		SpringApplication.run(GeneratePOJOApplication.class, args).close();
 		GeneratePOJOCommands gPOJOcomm = new GeneratePOJOCommands(args);
-		gPOJOcomm.execute();
+		try {
+			gPOJOcomm.execute();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
--- src/main/java/com/dejans/GeneratePOJO/GeneratePOJOCommands.java
+++ src/main/java/com/dejans/GeneratePOJO/GeneratePOJOCommands.java
@@ -4,10 +4,13 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.io.FileUtils;
+import org.springframework.ui.Model;
 
 import freemarker.template.Configuration;
 import freemarker.template.Template;
@@ -15,6 +18,8 @@ import freemarker.template.TemplateException;
 import freemarker.template.TemplateExceptionHandler;
 
 import com.dejans.model.LoadModel;
+import com.dejans.model.TreeMapYamlParse;
+import com.dejans.model.ModelDefinitionTree;
 
 public class GeneratePOJOCommands {
     private String[] args;
@@ -22,10 +27,31 @@ public class GeneratePOJOCommands {
     private String baseDirProject;
     private String baseDirPOJOFiles;
     private String baseDirModelFiles;
+    private String baseDirTemplate;
     private String nameProject;
     private String newProjectTemplate;
+    final String fS = File.separator;
     // Short name of project witout special character (-,#,$). It use as a java variable
     private String name;
+
+    public class Items {
+        private String item;
+        private String type;
+
+        private Items (String item, String type) {
+            this.item = item;
+            this.type = type;
+        }
+
+		public String getItem() {
+			return item;
+		}
+
+		public String getType() {
+			return type;
+		}
+
+    }
 
     public GeneratePOJOCommands(String[] args) {
         if (args.length <= 0) {
@@ -47,22 +73,51 @@ public class GeneratePOJOCommands {
                                 + (name.substring(ind + 1).length() > 1 ? name.substring(ind + 2) : "") : "");
             }
         }
-        String fS = File.separator;
         this.baseDirProject = baseDir + fS + "source" + fS + nameProject;
-        this.baseDirPOJOFiles = this.baseDirProject + fS + "src" + fS + "main" + fS + "java" + fS + "com" + fS + "dejans" + fS 
-                + name;
-        this.baseDirModelFiles = baseDir + "/source/Files";
+        this.baseDirTemplate = baseDir + fS + "generate-POJO-template";
+        this.baseDirPOJOFiles = this.baseDirProject + fS + "src" + fS + "main" + fS + "java" + fS + "com" + fS
+                + "dejans" + fS + name;
+        this.baseDirModelFiles = baseDir + fS + "source" + fS + nameProject + fS + "Files";
     }
 
-    public void execute() {
-
+    public void execute() throws IOException {
+        LoadModel model = null;
         try {
-            LoadModel model = new LoadModel(baseDirModelFiles);
+            model = new LoadModel(baseDirModelFiles);
         } catch (IOException e1) {
             // TODO Auto-generated catch block
             e1.printStackTrace();
         }
 
+        TreeMapYamlParse ymlModel = model.getTMYP();
+        ModelDefinitionTree modelTree = new ModelDefinitionTree(ymlModel);
+
+        // Create and adjust the configuration singleton 
+        File fileDirTemp = new File(baseDirTemplate);
+        Configuration cfg = new Configuration(Configuration.VERSION_2_3_27);
+        cfg.setDirectoryForTemplateLoading(fileDirTemp);
+        cfg.setDefaultEncoding("UTF-8");
+        cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+        cfg.setLogTemplateExceptions(false);
+        cfg.setWrapUncheckedExceptions(true);
+
+
+        String obj = new String();
+        obj = modelTree.nextObject(obj);
+        while (obj != null) {
+            String item = null;
+            System.out.println(obj);
+            String modelType = modelTree.getModelsType(obj);
+            if (modelType.equals("object")) {
+                generatePOJOObject(modelTree, obj, cfg);
+
+            } else if (modelType.equals("List") || modelType.equals("Array")) {
+
+            }
+            obj = modelTree.nextObject(obj);
+        }
+
+        /*        
         boolean de = new File(baseDirProject).exists();
         if (de) {
             System.out.println("Project (directory or file) " + nameProject + " exists.");
@@ -74,19 +129,19 @@ public class GeneratePOJOCommands {
             }
             try {
                 File destFile = new File(baseDirProject);
-                /* Create and adjust the configuration singleton */
+                // Create and adjust the configuration singleton 
                 Configuration cfg = new Configuration(Configuration.VERSION_2_3_27);
                 //cfg.setDirectoryForTemplateLoading(new File("Files"));
                 cfg.setDefaultEncoding("UTF-8");
                 cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
                 cfg.setLogTemplateExceptions(false);
                 cfg.setWrapUncheckedExceptions(true);
-
+        0
                 Map root = new HashMap();
                 root.put("basedir", "${basedir}");
                 root.put("name", name);
                 root.put("nameProject", nameProject);
-
+        
                 File sourceFile = new File(newProjectTemplate);
                 subdirecotories(sourceFile, destFile, newProjectTemplate, baseDirProject, cfg, root);
             } catch (IOException e) {
@@ -94,6 +149,46 @@ public class GeneratePOJOCommands {
                 e.printStackTrace();
             }
         }
+        */
+    }
+
+    private void generatePOJOObject(ModelDefinitionTree modelTree, String obj, Configuration cfg) {
+        String item = null;
+        List<Items> items = new ArrayList<Items>();
+        try {
+            File fileTemp = new File(baseDirTemplate, "classTempObject.java.ftl");
+            Template temp = cfg.getTemplate(fileTemp.getName());
+                File file = new File(baseDirPOJOFiles + fS + obj + ".java");
+
+            Map root = new HashMap();
+            root.put("basedir", "${basedir}");
+            root.put("className", obj);
+            root.put("nameProject", nameProject);
+            root.put("name", name);
+
+            item = modelTree.nextItem(obj, item);
+            while (item != null) {
+                System.out.println(item);
+                String type = modelTree.getItemJavaType(obj, item);
+                Items itemT = new Items (item, type);
+                items.add(itemT);
+                item = modelTree.nextItem(obj, item);
+            }
+            root.put("items",items);
+
+            BufferedWriter out = new BufferedWriter(new FileWriter(file, false));
+            try {
+                temp.process(root, out);
+            } catch (TemplateException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
     }
 
     private void subdirecotories(File fileCurrent, File destFile, String sourceBase, String destinationBase,
